a63e32ff126ba4d60f72caaa47093c4bf276203d
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2025-11-10 17:41:08 +0700

    Implement weather checking functionality with API integration and event listeners

---
+++ Weather_App/script.js
@@ -0,0 +1,49 @@
+const apiKey = 'ddbae39683894f6195710323b4dd38c0';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    if (response.status === 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main === "Clouds") {
+            weatherIcon.src = "assets/clouds.png"
+        } else if (data.weather[0].main === "Clear") {
+            weatherIcon.src = "assets/clear.png"
+        } else if (data.weather[0].main === "Drizzle") {
+            weatherIcon.src = "assets/drizzle.png"
+        } else if (data.weather[0].main === "Mist") {
+            weatherIcon.src = "assets/mist.png"
+        } else if (data.weather[0].main === "Rain") {
+            weatherIcon.src = "assets/rain.png"
+        } else if (data.weather[0].main === "Snow") {
+            weatherIcon.src = "assets/snow.png"
+        };
+
+
+        document.querySelector(".weather").style.display = "block";
+    }
+};
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+});
