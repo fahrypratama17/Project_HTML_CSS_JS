ee8337ecdb1e9c6d5dcaa819cfeed51808905d61
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-01-16 20:41:16 +0700

    Add anime card template to script.js

---
+++ Anime_Website/script.js
@@ -0,0 +1,13 @@
+const card = `          
+                      <div class="anime-card">
+                        <div class="anime-poster" style="background-image: url('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhpQEss6dHb6ub6eR-AREkCnAKdMeiNK2xGmpMytnI3nTHAbcWHJkxPVrd-DDnyu0pH5SSzEbdsGujUm9y2YFy4Z8Bq6hTrLawzcHWm6WyZTI9a7UStuHL7y-f3Q9MKzPMXrFZMeiYcAuE/s1600/Koe-no-Katachi-Anime-Visual.jpg')" ></div>
+                        <div class="anime-info">
+                          <div class="anime-info">
+                            <div class="anime-title">Koe no Katachi</div>
+                            <div class="anime-rating"><i class="fas fa-star"></i> 4.9</div>
+                            <div class="anime-genres">Romance</div>
+                          </div>
+                        </div>
+                      </div>
+                    `
+
