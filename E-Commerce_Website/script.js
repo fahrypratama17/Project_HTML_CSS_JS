0194a1e64b4f75524df1a43b6ce47462f8a94596
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-01-18 02:29:49 +0700

    Add product rendering functions and error handling in script.js

--- E-Commerce_Website/script.js
+++ E-Commerce_Website/script.js
@@ -9,10 +9,61 @@ async function loadProducts() {
       throw new Error('Error fetching the data...');
     }
     products = await response.json();
+    console.log('data product:',products);
+
+    renderFeaturedProducts();
+    renderProducts();
 
   } catch (error) {
-
+      console.error('Error Loading products...', error);
   }
 }
 
-console.log(products)
+loadProducts()
+
+
+function renderFeaturedProducts() {
+  const container = document.getElementById('featuredProducts');
+  const featured = products.slice(0, 2);
+
+  container.innerHTML = featured.map((product, index) =>
+    `
+      <div class="featured-product">
+        <div class="product-badge ${index === 1 ? "dark" : ""}">
+          
+        </div>
+        
+        <button class="wishlist-btn" onclick="toggleWishlist(${product.id})">
+          <i class="far fa-heart"></i>
+        </button>
+      
+        <img src="${product.image}" alt="${product.title}" class="product-image">
+        <div class="product-info">
+          <h4>${product.title.substring(0, 30)}...</h4>
+          <button class="product-price ${index === 1 ? "dark" : ""}" onclick="addToCart(${product.id})">
+            $${product.price}
+          </button>
+        </div>
+      </div>
+    `
+  ).join("");
+}
+function renderProducts() {
+  const container = document.getElementById('productsGrid');
+  const filteredProducts = currentCategory === 'all'
+    ? products
+    : products.filter((p) => p.category === currentCategory);
+
+  if (filteredProducts.length === 0) {
+    container.innerHTML =
+      `  
+        <div class="loading">
+          <p>No products found</p>
+        </div>  
+      `
+    ;
+  }
+
+  container.innerHTML =
+}
+
